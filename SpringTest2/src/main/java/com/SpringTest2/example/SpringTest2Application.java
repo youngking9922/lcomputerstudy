c30eccbc942fcd88cfba85978a66bdcec63221e0
youngking9922 <youngking9922@gmail.com>
2022-03-30 20:09:24 +0900

    프로젝트추가

---
+++ SpringTest2/src/main/java/com/SpringTest2/example/SpringTest2Application.java
@@ -0,0 +1,13 @@
+package com.SpringTest2.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTest2Application.class, args);
+	}
+
+}
---
+++ SpringTest2/src/test/java/com/SpringTest2/example/SpringTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringTest2.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
